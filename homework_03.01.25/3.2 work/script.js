6dc56544ff6bf0a818085617200fdb5c9879d633
oleksijermantraut <mawabek43@gmail.com>
2025-01-05 18:26:55 +0200

    add console.log

--- homework_03.01.25/3.2 work/script.js
+++ homework_03.01.25/3.2 work/script.js
@@ -5,6 +5,6 @@ const ageto=document.getElementById('age').value;
 document.getElementById('agejs').textContent=ageto;
 const profto=document.getElementById('prof').value;
 document.getElementById('profjs').textContent=profto;
-  document.getElementById("output").style.display = "block";
+document.getElementById("output").style.display = "block";
+console.log(`Привіт! Мене звати ${nameto}. Мені ${ageto} років. Моя професія ${profto}.`);
 };
-
