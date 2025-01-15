36b052ac0c3afd34a2ed2bb208a2fba6ac96726e
oleksijermantraut <mawabek43@gmail.com>
2025-01-15 08:15:36 +0200

    change return to breack

--- homework_14.01.25/work_5.3/script.js
+++ homework_14.01.25/work_5.3/script.js
@@ -2,7 +2,7 @@ let N = (prompt("Введіть ціле число"));
 for (let i = 1; i <= 100; i++) {
     if (N === null) {
         console.log(`Ви відмовились вводити число`);
-        return;
+        break;
     }
     if (i * i <= N) {
         console.log(i);
