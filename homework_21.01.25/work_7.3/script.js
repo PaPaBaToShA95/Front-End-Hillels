d9b09200bcefa33e010cc0d0f94ff4cc4459ba88
oleksijermantraut <mawabek43@gmail.com>
2025-01-21 23:10:19 +0200

    work 7.3 done

--- homework_21.01.25/work_7.3/script.js
+++ homework_21.01.25/work_7.3/script.js
@@ -1,19 +1,33 @@
 for (let i = 0; i < 10; i++) {
-    let num = prompt("Введіть число від 10 до 100");
+    let num = prompt("Введіть число більше 100");
+
+
+    if (num === null) {
+        console.log("Ви відмовились вводити число");
+        break;
+    }
+
     let numb = parseInt(num, 10);
 
-    if (isNaN(numb) || num === null) {
-        console.log("Ви відмовились вводити число або ввели не число");
+
+    if (isNaN(numb)) {
+        console.log(`Ви ввели: ${num}`);
+        break;
     }
+
+
     if (numb < 100) {
-        console.log(`Ви ввели число менше 100, введіть ще раз`)
+        console.log("Ви ввели число менше 100, введіть ще раз");
     }
+
+
     if (numb > 100) {
-        console.log(`Ви ввели ${num}`)
-        break
+        console.log(`Ви ввели число: ${num}`);
+        break;
     }
-    if (i === 9)
-        console.log(`Ви ввели ${num}`)
 
 
+    if (i === 9) {
+        console.log(`Ви ввели число: ${num}`);
+    }
 }
