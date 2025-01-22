50a361f37f028766f5dd67bca51c188ed5f330fa
oleksijermantraut <mawabek43@gmail.com>
2025-01-22 14:35:17 +0200

    add new works

--- homework_21.01.25/Practic/script.js
+++ homework_21.01.25/Practic/script.js
@@ -47,7 +47,47 @@ const multNumb = numbers.reduce((acc, currentValue) => {
 console.log(multNumb)
 
 
+const numbers = [1, 2, 2, 3, 4, 4, 4, 5, 6, 6, 6, 6];
 
+// Очікуваний результат: {1: 1, 2: 2, 3: 1, 4: 3, 5: 1, 6: 4}
+const num = numbers.reduce((acc, currentValue) => {
+    if (currentValue in acc) {
+        acc[currentValue] += 1;
+    }
+    else {
+        acc[currentValue] = 1
+    }
+    return acc;
+}, {})
+num
+
+const cart = [
+    { price: 100, quantity: 2 },
+    { price: 50, quantity: 1 },
+    { price: 20, quantity: 4 },
+];
+
+// Очікуваний результат: 330
+
+const totalPrice = cart.reduce((acc, currentValue) => {
+    acc += currentValue.price * currentValue.quantity;
+    return acc;
+}, 0);
+totalPrice;
+
+
+const sentence = "JavaScript is amazing";
+
+// Очікуваний результат: 7 (голосні: a, a, i, i, a, a, i)
+
+const liters = sentence.split("").reduce((acc, currentValue) => {
+    if ('aeiouAEIOU'.includes(currentValue)) {
+        acc += 1;
+    }
+    return acc;
+}, 0);
+
+liters
 
 
 
