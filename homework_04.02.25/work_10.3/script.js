8cc16bd7ee9212f8064a0d44a89091a3cbdb1f84
oleksijermantraut <mawabek43@gmail.com>
2025-02-07 19:09:14 +0200

    add folder

--- homework_04.02.25/work_10.3/script.js
+++ homework_04.02.25/work_10.3/script.js
@@ -1,21 +1,54 @@
-const contactBase = [
-    {
-        name: `Олексій`,
-        number: `+380936064694`,
-        email: `mawabel@ukr.net`
+const contactBook = {
+    contacts: [
+        {
+            name: 'Олексій Ермантраут',
+            phone: '+380501234567',
+            email: 'oleksii@gmail.com'
+        },
+        {
+            name: 'Анна Сидорова',
+            phone: '+380673456789',
+            email: 'anna.sydorova@gmail.com'
+        },
+        {
+            name: 'Іван Іванов',
+            phone: '+380932345678',
+            email: 'ivan.ivanov@gmail.com'
+        }
+    ],
+    addContact(newName, newPhone, newEmail) {
+        this.contacts.push({
+            name: newName,
+            phone: newPhone,
+            email: newEmail
+        });
+    },
+    contactList() {
+        this.contacts.forEach(contact => {
+            console.log(`Ім'я: ${contact.name}, Телефон: ${contact.phone}, Пошта: ${contact.email}`);
+        });
+    },
+    searchContacts(partialName) {
+        const matchingContacts = this.contacts.filter(contact =>
+            contact.name.toLowerCase().includes(partialName.toLowerCase())
+        );
+        if (matchingContacts.length > 0) {
+            matchingContacts.forEach(contact => {
+                console.log(`Ім'я: ${contact.name}, Телефон: ${contact.phone}, Пошта: ${contact.email}`);
+            });
+        } else {
+            console.log("Збігів не знайдено, спробуйте ввести дані ще раз");
+        }
     }
-];
-const addCont = (NewName, NewNumber, NewEmail) => {
-    const newContact = {
-        name: NewName,
-        number: NewNumber,
-        email: NewEmail
-    };
-    contactBase.push(newContact);
-}
+};
 
 
+contactBook.addContact('Вася Пупкін', '+380932957409', 'vasya.pupkin@gmail.com');
 
 
-contactBase
-contactBase.newContact(`Masha`, `145t`, `segd`)
+contactBook.contactList();
+
+contactBook.searchContacts('ан');
+contactBook.searchContacts('Іван');
+contactBook.searchContacts('пуп');
+contactBook.searchContacts('петро');
