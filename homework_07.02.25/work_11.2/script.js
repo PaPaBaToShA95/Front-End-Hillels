ff07641b4101bc9b34b514e7672e7e82bdf740e2
oleksijermantraut <mawabek43@gmail.com>
2025-02-08 01:04:14 +0200

    work 11.1 done

--- homework_07.02.25/work_11.2/script.js
+++ homework_07.02.25/work_11.2/script.js
@@ -1,21 +1,19 @@
-const contactBase = [
-    {
-        name: `Олексій`,
-        number: `+380936064694`,
-        email: `mawabel@ukr.net`
+const text = document.querySelector('.homework__text');
+const button = document.querySelector('.color__button');
+const button2 = document.querySelector('.size__button');
+button.addEventListener('click', () => {
+    if (text.style.color === 'black') {
+        text.style.color = 'red';
     }
-];
-const addCont = (NewName, NewNumber, NewEmail) => {
-    const newContact = {
-        name: NewName,
-        number: NewNumber,
-        email: NewEmail
-    };
-    contactBase.push(newContact);
-}
-
-
-
-
-contactBase
-contactBase.newContact(`Masha`, `145t`, `segd`)
+    else {
+        text.style.color = 'black';
+    }
+});
+button2.addEventListener('click', () => {
+    if (text.style.fontSize === '20px') {
+        text.style.fontSize = '45px';
+    }
+    else {
+        text.style.fontSize = '20px';
+    }
+});
