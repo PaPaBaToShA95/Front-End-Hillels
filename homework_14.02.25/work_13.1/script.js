fe622ef3fa2d70612012c9c5fb62b13660d93506
oleksijermantraut <mawabek43@gmail.com>
2025-02-15 04:00:30 +0200

    work 13.1 done

---
+++ homework_14.02.25/work_13.1/script.js
@@ -0,0 +1,54 @@
+const form = document.getElementById('formMy');
+const btn = document.getElementById('btn');
+const errorms = document.getElementById('errorMessage');
+const errorEmail = document.getElementById('errorEmail');
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+btn.addEventListener('click', function (event) {
+    event.preventDefault();
+    errorms.textContent = "";
+    errorEmail.style.display = 'none';
+
+    const formData = new FormData(form);
+    const userName = formData.get('Name');
+    const userMessage = formData.get('Message');
+    const userNumber = formData.get('Number');
+    const userEmail = formData.get('Email');
+    if (userName === "") {
+        errorms.textContent = "Ви не ввели Ім'я";
+        return;
+    }
+    if (userMessage.length > 0 && userMessage.length < 5) {
+        errorms.textContent = "Довжина повідомлення має бути не менше 5 символів";
+        return;
+    }
+    if (userNumber === "") {
+        errorms.textContent = "Ви не ввели Номер телефону";
+        return;
+    }
+    if (userEmail === "") {
+        errorms.textContent = "Ви не ввели Email";
+        return;
+    }
+
+    if (!emailRegex.test(userEmail)) {
+        errorEmail.style.display = 'flex';
+        return;
+    }
+    if (userMessage.length === 0) {
+        console.log(`Введені дані:
+    Ім'я: ${userName}
+    Номер телефону: ${userNumber} 
+    Email: ${userEmail}
+    Повідомлення не було додане`);
+        form.reset();
+    }
+    if (userMessage.length != 0) {
+        console.log(`Введені дані:
+    Ім'я: ${userName}
+    Повідомлення: ${userMessage}
+    Номер телефону: ${userNumber} 
+    Email: ${userEmail}`);
+        form.reset();
+    }
+});
