e7a54781c0be3ab41a1556fb3c768c88698c2a33
oleksijermantraut <mawabek43@gmail.com>
2025-02-28 21:16:40 +0200

    work 17.1 done

--- homework_28.02.25/work_17.1/scruot.js
+++ homework_28.02.25/work_17.1/scruot.js
@@ -26,3 +26,7 @@ console.log(calc.add(5, 3));
 console.log(calc.subtract(10, 4));
 console.log(calc.multiply(3, 6)); 
 console.log(calc.divide(8, 2)); 
+
+
+
+// Не знаю чи це дуже чесно з мого боку, але в попередній 16 лекції цей код вже був, я його трохи підправив і власне готово)
