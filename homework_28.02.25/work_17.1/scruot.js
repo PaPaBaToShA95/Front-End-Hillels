466448bf2a02ca40ccfa1c59dec4f9e9d1d3b450
oleksijermantraut <mawabek43@gmail.com>
2025-02-28 21:08:19 +0200

    1

--- homework_28.02.25/work_17.1/scruot.js
+++ homework_28.02.25/work_17.1/scruot.js
@@ -22,7 +22,7 @@ function Calculator() {
 
 const calc = new Calculator();
 
-console.log(calc.add(5, 3)); // 8
-console.log(calc.subtract(10, 4)); // 6
-console.log(calc.multiply(3, 6)); // 18
-console.log(calc.divide(8, 2)); // 4
+console.log(calc.add(5, 3)); 
+console.log(calc.subtract(10, 4));
+console.log(calc.multiply(3, 6)); 
+console.log(calc.divide(8, 2)); 
