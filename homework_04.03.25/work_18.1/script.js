a3b4dbc3115f09ec7f0b85eca82132f23d1c4195
oleksijermantraut <mawabek43@gmail.com>
2025-03-04 19:48:26 +0200

    work 18.1 done

--- homework_04.03.25/work_18.1/script.js
+++ homework_04.03.25/work_18.1/script.js
@@ -1,41 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
     const setTimerBtn = document.querySelector('.setTimer__btn');
-    const timer = document.querySelector('.timer');
     const hour = document.querySelector('#hour');
     const min = document.querySelector('#min');
     const sec = document.querySelector('#sec');
 
     setTimerBtn.addEventListener('click', () => {
-        const hourValue = document.querySelector('.setTimer__hour').value;
-        const minValue = document.querySelector('.setTimer__min').value;
-        const secValue = document.querySelector('.setTimer__sec').value;
+        let hourValue = document.querySelector('.setTimer__hour').value;
+        let minValue = document.querySelector('.setTimer__min').value;
+        let secValue = document.querySelector('.setTimer__sec').value;
 
-        hour.textContent = hourValue;
-        min.textContent = minValue;
-        sec.textContent = secValue;
+        hour.textContent = formatTime(hourValue);
+        min.textContent = formatTime(minValue);
+        sec.textContent = formatTime(secValue);
     });
 });
 
-
 document.addEventListener('DOMContentLoaded', () => {
-    const timer = document.querySelector('.timer');
     const hour = document.querySelector('#hour');
     const min = document.querySelector('#min');
     const sec = document.querySelector('#sec');
 
     setInterval(() => {
         if (sec.textContent > 0) {
-            sec.textContent--;
+            sec.textContent = formatTime(--sec.textContent);
         } else if (min.textContent > 0) {
-            min.textContent--;
+            min.textContent = formatTime(--min.textContent);
             sec.textContent = 59;
         } else if (hour.textContent > 0) {
-            hour.textContent--;
+            hour.textContent = formatTime(--hour.textContent);
             min.textContent = 59;
             sec.textContent = 59;
         }
     }, 1000);
 });
 
-
- 
+function formatTime(value) {
+    return value.toString().padStart(2, '0');
+}
